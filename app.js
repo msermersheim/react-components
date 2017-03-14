6aa3e2cf33b55e0c53657005bf45d94573a98d23
Michael Sermersheim <m.sermersheim@gmail.com>
2017-03-13 21:37:14 -0700

    Basic grocery item list - STATELESS functional components

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList groceries={['apples', 'bananas', 'oranges', 'grapes']}/>
+    <GroceryListItem item={['mango']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    <li onClick={onListItemClick}>{props.groceries[0]}</li>
+    <li>{props.groceries[1]}</li>
+    <li>{props.groceries[2]}</li>
+    <li>{props.groceries[3]}</li>
+  </ul>
+);
+
+
+var onListItemClick = (event) => {
+  console.log('Item was clicked');
+};
+
+var GroceryListItem = (props) => (
+  <li>{props.item[0]}</li>
+);
+
+var Grapes = () => (
+  <li>grapes</li>
+);
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
